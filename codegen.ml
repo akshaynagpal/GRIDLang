57be8df1a9f0c2539572ca5d98fb2a6dabe796ca
Akshay Nagpal <akshay2626@gmail.com>
2017-03-01 17:49:10 -0500

    init codegen

---
+++ codegen.ml
@@ -0,0 +1,11 @@
+(* Code generation: translate takes a semantically checked AST and
+produces LLVM IR
+
+http://llvm.org/docs/tutorial/index.html
+
+Detailed documentation on the OCaml LLVM library:
+
+http://llvm.moe/
+http://llvm.moe/ocaml/
+
+*)
