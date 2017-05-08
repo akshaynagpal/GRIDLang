cbefde219a9cf4784b6bf3df33072192578e535c
Sagar Damani <sagardmni@gmail.com>
2017-05-08 15:23:03 -0400

    Make triggerRule take typetag rather than listNode

--- preprocess.ml
+++ preprocess.ml
@@ -14,10 +14,10 @@ let insideItemStruct = ref false
 let exitingItemStruct = ref false
 let playerStructBraceCounter = ref 0
 let listNode = ref "Item listNode\n{\n"
-let triggerRule = ref "int triggerRule(int src_x, int src_y, int dst_x, int dst_y, *Item listNode l)\n{\nint x;	if (l == None)\n{\n		print(\"No source item\");\nreturn 0;\n}"
+let triggerRule = ref "int triggerRule(int src_x, int src_y, int dst_x, int dst_y, string typetag)\n{\nint x;\n"
 
 let append_to_string structName = 
-	triggerRule := !triggerRule ^ "if (l.typetag == \"" ^ structName ^"\")\n{\nx = "^structName^"rule(src_x, src_y, dst_x, dst_y);\nreturn x;\n}\n"
+	triggerRule := !triggerRule ^ "if (typetag == \"" ^ structName ^"\")\n{\nx = "^structName^"rule(src_x, src_y, dst_x, dst_y);\nreturn x;\n}\n"
 
 let process_files filename1 =
 	let contains s1 s2 =
